c295aab23c271a7151e99faad8209c9b5cf8d3a9
Guillaume Qu <guillaume.qu.1@etu.sorbonne-universite.fr>
2026-02-08 20:22:07 +0100

    we still s*ck just like before

--- robot_challenger.py
+++ robot_challenger.py
@@ -84,12 +84,12 @@ class Robot_player(Robot):
                 rotation = (random.random() - 0.5) * 0.01
                 self.memory = self.log_sum_of_translation
                 
-
+            '''
             print("front =", sensors[sensor_front],")  left = ",sensors[sensor_left], " right = ", sensors[sensor_right], "f left = ", sensors[sensor_front_left], "f rightt = ", sensors[sensor_front_right])
             print("rotation = ", rotation, " translation", translation, "\n")
             print(self.x,"    ",self.y)
             print("sum tran", self.log_sum_of_translation, " mem =",self.memory)
-
+            '''
         elif self.robot_id == 3 :
             translation = sensors[sensor_front]*0.5 # A MODIFIER
             rotation = 0.5 # A MODIFIER
